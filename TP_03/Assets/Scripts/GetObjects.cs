53e86ef5bd0b390564b067a64e3e0b730ca9ab83
GaracLui <158767416+GaracLui@users.noreply.github.com>
2024-05-20 12:59:05 -0300

    Beginning

---
+++ TP_03/Assets/Scripts/GetObjects.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetObjects : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) // Metodo para saber cuando hemos colisionado con un objeto
+    {
+        if (other.CompareTag("Alimento")) // Compara con las etiquetas que hemos agragedo a la lista de Tags
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Bien, asi debes alimentarte"); // Imprime mensaje por pantalla
+            //AumentasEnergia;
+        }
+
+        if (other.CompareTag("Alimento/No Saludable")) // Compara con las etiquetas que hemos agragedo a la lista de Tags
+        {
+            Destroy(other.gameObject);
+            Debug.Log("Mal, asi dañas tu salud"); // Imprime mensaje por pantalla
+            //DisminuyesEnergia
+        }
+
+        /*AumentasEnergia()
+        {
+
+        }
+
+        DisminuyesEnergia()
+        {
+
+        }*/
+
+    }
+
+}
