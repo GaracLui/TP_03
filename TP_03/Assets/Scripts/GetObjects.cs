09f745747ed684f8c1561a27a2d37a4a94b215d7
GaracLui <158767416+GaracLui@users.noreply.github.com>
2024-05-30 13:01:07 -0300

    10ma Clase Virtual

--- TP_03/Assets/Scripts/GetObjects.cs
+++ TP_03/Assets/Scripts/GetObjects.cs
@@ -1,34 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GetObjects : MonoBehaviour
 {
+    public int foodCount;
+    public int maxFood;
+    public int drinksCount;
+    public int maxDrinks;
+
+    public TextMeshProUGUI numberFood;
+    public TextMeshProUGUI numberDrinks;
+
+    private void Update()
+    {
+        numberFood.text = foodCount.ToString();
+        numberDrinks.text = drinksCount.ToString();
+
+    }
+
+
     private void OnTriggerEnter(Collider other) // Metodo para saber cuando hemos colisionado con un objeto
     {
-        if (other.CompareTag("Alimento")) // Compara con las etiquetas que hemos agragedo a la lista de Tags
+        if (other.CompareTag("Alimento") && foodCount < maxFood) // Compara con las etiquetas que hemos agragedo a la lista de Tags
         {
             Destroy(other.gameObject);
-            Debug.Log("Bien, asi debes alimentarte"); // Imprime mensaje por pantalla
-            //AumentasEnergia;
+            //Debug.Log("Bien, asi debes alimentarte"); // Imprime mensaje por pantalla
+            foodCount++;
         }
 
         if (other.CompareTag("Alimento/No saludable")) // Compara con las etiquetas que hemos agragedo a la lista de Tags
         {
             Destroy(other.gameObject);
-            Debug.Log("Mal, asi dañas tu salud"); // Imprime mensaje por pantalla
-            //DisminuyesEnergia
+            //Debug.Log("Mal, asi dañas tu salud"); // Imprime mensaje por pantalla
+            foodCount--;
         }
 
-        /*AumentasEnergia()
+        if (other.CompareTag("Bebidas") && drinksCount < maxDrinks)
         {
+            Destroy(other.gameObject);
 
+            drinksCount++;
         }
-
-        DisminuyesEnergia()
-        {
-
-        }*/
 
     }
 
